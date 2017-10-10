70892537a311def9dc6a72d6ff346c2c850a8f26
Ruslan Kladko <rkladko@gmail.com>
2017-10-10 17:15:51 +0300

    Dont use eval fix

--- 5.js
+++ 5.js
@@ -1,28 +1,56 @@
-function evalo(digit, operation) {
-    return operation ? eval(digit+operation) : digit
+function evalFor(value, param) {
+    if (typeof param === 'object') {
+        switch (param.operation) {
+            case '+':
+                return value + param.operand;
+                break;
+            case '-':
+                return value - param.operand;
+                break;
+            case '*':
+                return value * param.operand;
+                break;
+            case '/':
+                return value / param.operand;
+                break;
+        }
+    } else {
+        return value;
+    }
 }
-function zero(x) { return evalo(0, x); }
-function one(x) { return evalo(1, x); }
-function two(x) { return evalo(2, x); }
-function three(x) { return evalo(3, x); }
-function four(x) {return evalo(4, x); }
-function five(x) { return evalo(5, x); }
-function six(x) {return evalo(6, x); }
-function seven() { return evalo(7, x); }
-function eight(x) { return evalo(8, x); }
-function nine(x) { return evalo(9, x); }
+
+function Operation(operation, operand) {
+    return {
+        operation: operation,
+        operand: operand
+    }
+}
+
+function zero(x) {return evalFor(0, x)}
+function one(x)  {return evalFor(1, x)}
+function two(x)  {return evalFor(2, x)}
+function three(x) {return evalFor(3, x)}
+function four(x)  {return evalFor(4, x)}
+function five(x)  {return evalFor(5, x)}
+function six(x)  {return evalFor(6, x)}
+function seven(x)  {return evalFor(7, x)}
+function eight(x)  {return evalFor(8, x)}
+function nine(x)  {return evalFor(9, x)}
 
 function plus(x) {
-    return '+' + x;
+    return Operation('+', x);
 }
+
 function minus(x) {
-    return '-' + x;
+    return Operation('-', x);
 }
+
 function times(x) {
-    return '*' + x;
+    return Operation('*', x);
 }
+
 function dividedBy(x) {
-    return '/' + x;
+    return Operation('/', x);
 }
 
 function checkCalculations() {
